7e42458d7e37c2f8f5ba1dc3fdd94c1e60c8e140
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-01-26 12:47:38 +0100

    Fin de sección 4 - Añadiendo hook useState en ItemCounter y estilos CSS

--- src/shopping-cart/ItemCounter.tsx
+++ src/shopping-cart/ItemCounter.tsx
@@ -1,3 +1,10 @@
+import { useState } from "react";
+
+// Importación desde archivo
+// import "./ItemCounter.css";
+// Importación como módulo
+import styles from "./ItemCounter.module.css";
+
 // Componente con propiedades
 interface Props {
   name: string;
@@ -5,35 +12,52 @@ interface Props {
 }
 
 // rafc -> Crea el componente
-export const ItemCounter = ({ name, quantity }: Props) => {
+export const ItemCounter = ({ name, quantity = 1 }: Props) => {
   // Muestra cada nombre dos veces, por efecto del StrictMode
   // console.log(name);
 
+  const [count, setCount] = useState(quantity);
+
   // Función auxiliar para gestionar onClick
   // separando el template de la lógica
-  const handleClick = () => {
+  /* const handleClick = () => {
     console.log(`Click en ${name}`);
+  }; */
+
+  const handleAdd = () => {
+    setCount(count + 1);
+  };
+
+  const handleSubtract = () => {
+    if (count === 1) return;
+
+    setCount(count - 1);
   };
 
   return (
     <section
-      style={{
-        display: "flex",
-        alignItems: "center",
-        gap: 10,
-        marginTop: 10,
-      }}
+      // className="item-row"     // como archivo
+      className={styles.itemRow}  // como módulo, nombre de clase en camel case
+      //   style={{
+      //     display: "flex",
+      //     alignItems: "center",
+      //     gap: 10,
+      //     marginTop: 10,
+      //   }}
     >
       <span
+        //className="item-text" // como archivo
+        className={styles['item-text']}  // como módulo, nombre de clase con guiones
         style={{
-          width: 150,
+          //width: 150,
+          color: count === 1 ? "red" : "black",
         }}
       >
         {name}
       </span>
-      <button onClick={handleClick}>+1</button>
-      <span>{quantity}</span>
-      <button>-1</button>
+      <button onClick={handleAdd}>+1</button>
+      <span>{count}</span>
+      <button onClick={handleSubtract}>-1</button>
     </section>
   );
 };
